fd80292c90948171d20870da55c1590f9e0d52a1
Vobh <vobh_malino@hotmail.com>
2025-02-05 11:36:01 -0300

    Setting rota url for users

--- natureggae_backend/urls.py
+++ natureggae_backend/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/products/', include ('product.urls')),
+    path('api/auth/', include('useraccount.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
